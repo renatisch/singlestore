b2376183091a7b77475e3cc285e916af03067b33
Renat Ishmukhamedov <renat.isch@gmail.com>
2024-05-20 17:25:52 +0200

    petrs token

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def read_root():
 
 @app.get("/.well-known/singlestore-verify/token")
 def read_root():
-    return "f3e80765-f223-429f-b3a5-d33ac8c039bd"
+    return "0394c6e0-d4cb-4432-9daa-b9915f32b6d7"
